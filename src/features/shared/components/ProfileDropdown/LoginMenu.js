33e6cedbb16365d581713cb4be0ccdbd9381de70
HOMEDESK\HomeDesk <chuongxl@gmail.com>
2021-09-11 12:56:53 +0700

    implement the logou

--- src/features/shared/components/ProfileDropdown/LoginMenu.js
+++ src/features/shared/components/ProfileDropdown/LoginMenu.js
@@ -2,30 +2,28 @@ import React, { Component, Fragment } from 'react';
 import { DropdownItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import Language from '../../languages/Language';
+import { data } from 'jquery';
 
 export class LoginMenu extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isAuthenticated: true,
-      userName: '',
-    };
-  }
-
   render() {
-    const { isAuthenticated, userName } = this.state;
-    if (!isAuthenticated) {
+    const { authenticated } = this.context;
+
+    if (!authenticated) {
       const registerPath = '';
       const loginPath = '';
       return this.anonymousView(registerPath, loginPath);
     }
     const profilePath = '';
     const logoutPath = { pathname: '', state: { local: true } };
-    return this.authenticatedView(userName, profilePath, logoutPath);
+    return this.authenticatedView(profilePath, logoutPath);
   }
 
-  authenticatedView(userName, profilePath, logoutPath) {
+  authenticatedView(profilePath, logoutPath) {
+    const { authContext, getUserInfo } = this.context;
+    let userName = '';
+    getUserInfo().then((data) => {
+      userName = data.name;
+    });
     return (
       <>
         {userName && <p className="h6 p-2 pl-4">{userName}</p>}
@@ -38,7 +36,7 @@ export class LoginMenu extends Component {
           {Language.get('options')}
         </DropdownItem>
         <div className="border-top my-1" />
-        <DropdownItem tag={Link} to={logoutPath} className="small py-2">
+        <DropdownItem onClick={() => authContext.logout()} to={logoutPath} className="small py-2">
           <i className="bi bi-box-arrow-left mr-2" />
           {Language.get('logout')}
         </DropdownItem>
