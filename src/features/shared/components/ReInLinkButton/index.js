938683b195b04074a7fea8810b6c479724837fea
Quang Dang <quang.dang@logixtek.com>
2021-11-29 17:36:05 +0700

    update style

--- src/features/shared/components/ReInLinkButton/index.js
+++ src/features/shared/components/ReInLinkButton/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useRouteMatch } from 'react-router-dom';
+import './style.scss';
 
 const { REACT_APP_RE_IN_CLOUD_URL } = process.env;
 
@@ -10,27 +11,9 @@ export default function ReInLinkButton() {
   const url = workId ? `${cloudUrl}/project/work/${workId}` : `${cloudUrl}/dashboard`;
 
   return (
-    <div className="dropdown mr-1 mt-2">
-      <a
-        target="_self"
-        href={url}
-        style={{
-          color: '#fff',
-          fontSize: 16,
-          padding: 8,
-        }}
-      >
-        <img
-          src="/img/rein_icon.ico"
-          alt="rein"
-          width="20"
-          height="20"
-          style={{
-            filter: 'brightness(0) invert(1)',
-            marginTop: -4,
-            marginRight: 4,
-          }}
-        />
+    <div className="dropdown mr-1 rein-button">
+      <a target="_self" href={url} className="rein-link">
+        <img src="/img/rein_icon.ico" alt="rein" className="rein-icon" />
         Re:In
       </a>
     </div>
